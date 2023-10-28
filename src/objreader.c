8b88bce6e4c2a470905a1caec161638ef5bd7392
zni <14016+zni@users.noreply.github.com>
2023-10-28 05:57:30 -0400

    Add debugging and tweak RT-11 OBJ code reader.

--- src/objreader.c
+++ src/objreader.c
@@ -24,30 +24,35 @@ int is_text_block(FILE *obj)
         fread(&chksum, sizeof(uint8_t), 1, obj);
         return 0;
     } else {
-        fseek(obj, -5, SEEK_CUR);
+        fseek(obj, -4, SEEK_CUR);
         return 1;
     }
 }
 
 void load_text_block(machine_state_t *machine, FILE *obj)
 {
-    uint8_t byte01, byte02;
+    uint8_t byte01, byte02, tag, pad;
     uint16_t word, len, load_offset, loc;
 
     fread(&byte01, sizeof(uint8_t), 1, obj);            // Read 01 00 data block tag.
     fread(&byte02, sizeof(uint8_t), 1, obj);
     fread(&len, sizeof(uint16_t), 1, obj);              // Read length word.   
-    fread(&byte01, sizeof(uint8_t), 1, obj);            // Read block type byte.
+    fread(&tag, sizeof(uint8_t), 1, obj);                // Read block type byte.
     fread(&load_offset, sizeof(uint16_t), 1, obj);      // Read load address word.
-    fseek(obj, 1, SEEK_CUR);                            // Skip past pad byte.
+    fread(&pad, sizeof(uint8_t), 1, obj);               // Skip past pad byte.
     len -= 8;                                           // Adjust the length.
     loc = load_offset;
+
+    printf("data block: %03o %03o\n", byte01, byte02);
+    printf("\tlen: %hu\n", len + 8);
+    printf("\ttag: %03o\n", tag);
+
     machine->memory->set_r(machine->memory, R_PC, load_offset);
     for (int i = 0; i < len; i += 2, loc += 2) {
         fread(&byte01, sizeof(uint8_t), 1, obj);
         fread(&byte02, sizeof(uint8_t), 1, obj);
         word = (byte02 << 8) | byte01;
-        printf("%07o: %07o\n", loc, word);
+        printf("%07o: %07o: %03o %03o\n", loc, word, byte01, byte02);
         machine->memory->direct_write_word(machine->memory, loc, word);
     }
 }
@@ -58,6 +63,7 @@ void load_program_from_obj(machine_state_t *machine, char* object_file)
 
     // Skip past other data blocks.
     while(!is_text_block(obj) && !feof(obj));
+
     if (!feof(obj)) {
         load_text_block(machine, obj);
     }
