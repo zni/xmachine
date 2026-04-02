9886729b9e7744284cbeae3e383a634abcbff209
zni <14016+zni@users.noreply.github.com>
2026-04-01 20:30:14 -0400

    Fix comment style in device_bus_mgr header.

--- src/libunibus/device_bus_mgr.h
+++ src/libunibus/device_bus_mgr.h
@@ -5,22 +5,22 @@
 
 #define SOCKET_DIR "/tmp/xmachine"
 
-// Bus init and cleanup.
+/* Bus init and cleanup. */
 bus_state* init_bus(char*, char*, char*);
 void cleanup_bus(bus_state*);
 int connect_cpu_bus(bus_state*);
 int connect_device_bus(bus_state*);
 
-// Priority functions.
+/* Priority functions. */
 void req_bus_master(bus_state*);
 void release_bus_master(bus_state*);
 
-// Master functions.
+/* Master functions. */
 uint16_t read_data_in(bus_state*, uint32_t);
 void write_data_out(bus_state*, uint32_t, uint16_t);
 void write_data_out_b(bus_state*, uint32_t, uint8_t);
 
-// Slave functions.
+/* Slave functions. */
 void data_bus_check(bus_state*, data_xfer_spec*);
 void data_bus_reply(bus_state*, data_xfer_spec*);
 
